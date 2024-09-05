93397ded08040d5e2239c8fd242ee0c95470bce8
tahsin soufizade <soufilearn@gmail.com>
2024-09-05 20:17:55 +0330

    remove header banner from another pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,82 +1,47 @@
 import { Link } from "react-router-dom";
 import loginSvg from "../assets/icons/login.svg";
-import headerImage from "../assets/images/IMG_20240707_131854.png";
 import { HiOutlineSun } from "react-icons/hi";
 
 const Header = () => {
   return (
-    <>
-      <div className="flex justify-between items-center px-10 py-4 m-4 rounded-lg bg-slate-900">
-        <nav>
-          <ul className="flex gap-6">
-            <li>
-              <Link to="/" className="transition-all hover:text-indigo-300">
-                صفحه اصلی
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="courses"
-                className="transition-all hover:text-indigo-300"
-              >
-                دوره ها
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="about-us"
-                className="transition-all hover:text-indigo-300"
-              >
-                درباره ما
-              </Link>
-            </li>
-          </ul>
-        </nav>
-
-        <div className="flex gap-6 ">
-          <button className="text-2xl flex w-10 h-10 rounded-full bg-slate-600 items-center justify-center">
-            <HiOutlineSun />
-          </button>
-
-          <Link
-            to="login"
-            className="flex gap-2 bg-indigo-200 hover:bg-indigo-100 py-2 px-4 rounded-md text-indigo-800 font-bold items-center"
-          >
-            <img src={loginSvg} alt="login" />
-            ورود
-          </Link>
-        </div>
-      </div>
-
-      <div className="px-20 flex items-center gap-10">
-        <section className="w-1/2">
-          <img src={headerImage} alt="soufi" className="w-5/6 -mr-20" />
-        </section>
-        <section className="w-1/2 text-center">
-          <h1 className="mb-6  font-dana-bold">
-            آموزش برنامه نویسی با حال خوب :){" "}
-          </h1>
-          <p className="text-xl">
-            با صوفی لرن برنامه نویسی رو با انرژی مثبت و حال خوب یاد بگیر
-          </p>
-
-          <div className="flex gap-4 items-center justify-center mt-10">
-            <Link
-              to="courses"
-              className="bg-indigo-600 hover:bg-indigo-700 py-2 px-4 rounded-md"
-            >
-              مشاهده دوره ها
+    <div className="flex justify-between items-center px-10 py-4 m-4 rounded-lg bg-slate-900">
+      <nav>
+        <ul className="flex gap-6">
+          <li>
+            <Link to="/" className="transition-all hover:text-indigo-300">
+              صفحه اصلی
             </Link>
-            <Link
-              to="login"
-              className="bg-slate-700/50 py-2 px-4 rounded-md hover:bg-slate-600/50"
-            >
-              ثبت نام
+          </li>
+          <li>
+            <Link to="courses" className="transition-all hover:text-indigo-300">
+              دوره ها
             </Link>
-          </div>
-        </section>
+          </li>
+          <li>
+            <Link
+              to="about-us"
+              className="transition-all hover:text-indigo-300"
+            >
+              درباره ما
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="flex gap-6 ">
+        <button className="text-2xl flex w-10 h-10 rounded-full bg-slate-600 items-center justify-center">
+          <HiOutlineSun />
+        </button>
+
+        <Link
+          to="login"
+          className="flex gap-2 bg-indigo-200 hover:bg-indigo-100 py-2 px-4 rounded-md text-indigo-800 font-bold items-center"
+        >
+          <img src={loginSvg} alt="login" />
+          ورود
+        </Link>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,8 +1,39 @@
 import CoursesList from "./CoursesList";
+import headerImage from "../../assets/images/IMG_20240707_131854.png";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <>
+      <div className="px-20 flex items-center gap-10">
+        <section className="w-1/2">
+          <img src={headerImage} alt="soufi" className="w-5/6 -mr-20" />
+        </section>
+        <section className="w-1/2 text-center">
+          <h1 className="mb-6  font-dana-bold">
+            آموزش برنامه نویسی با حال خوب :){" "}
+          </h1>
+          <p className="text-xl">
+            با صوفی لرن برنامه نویسی رو با انرژی مثبت و حال خوب یاد بگیر
+          </p>
+
+          <div className="flex gap-4 items-center justify-center mt-10">
+            <Link
+              to="courses"
+              className="bg-indigo-600 hover:bg-indigo-700 py-2 px-4 rounded-md"
+            >
+              مشاهده دوره ها
+            </Link>
+            <Link
+              to="login"
+              className="bg-slate-700/50 py-2 px-4 rounded-md hover:bg-slate-600/50"
+            >
+              ثبت نام
+            </Link>
+          </div>
+        </section>
+      </div>
+
       <CoursesList />
     </>
   );
