8f6f6dcebf6bf9df23b35f666767e4e3c2966c28
tahsin soufizade <soufilearn@gmail.com>
2024-09-05 12:48:04 +0330

    add & setup react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import ProductsList from "./components/ProductsList";
-// import RegisterForm from "./components/RegisterForm";
-// import VerifyForm from "./components/VerifyForm";
+import RegisterForm from "./components/RegisterForm";
+import VerifyForm from "./components/VerifyForm";
 
 function App() {
   return (
-    <>
-      {/* <RegisterForm />
-      <VerifyForm /> */}
-      <ProductsList />
-    </>
+    <Routes>
+      <Route path="/" element={<ProductsList />} />
+      <Route path="register" element={<RegisterForm />} />
+      <Route path="verify" element={<VerifyForm />} />
+    </Routes>
   );
 }
 
