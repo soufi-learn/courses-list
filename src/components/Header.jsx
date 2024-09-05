a343fd3a6befe0213335e19e617b20cb620fadc8
tahsin soufizade <soufilearn@gmail.com>
2024-09-05 16:47:37 +0330

    add dark/light button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import loginSvg from "../assets/icons/login.svg";
 import headerImage from "../assets/images/IMG_20240707_131854.png";
+import { HiOutlineSun } from "react-icons/hi";
 
 const Header = () => {
   return (
@@ -32,13 +33,19 @@ const Header = () => {
           </ul>
         </nav>
 
-        <Link
-          to="login"
-          className="flex gap-2 bg-indigo-200 hover:bg-indigo-100 py-2 px-4 rounded-md text-indigo-800 font-bold items-center"
-        >
-          <img src={loginSvg} alt="login" />
-          ورود
-        </Link>
+        <div className="flex gap-6 ">
+          <button className="text-2xl flex w-10 h-10 rounded-full bg-slate-600 items-center justify-center">
+            <HiOutlineSun />
+          </button>
+
+          <Link
+            to="login"
+            className="flex gap-2 bg-indigo-200 hover:bg-indigo-100 py-2 px-4 rounded-md text-indigo-800 font-bold items-center"
+          >
+            <img src={loginSvg} alt="login" />
+            ورود
+          </Link>
+        </div>
       </div>
 
       <div className="px-20 flex items-center gap-10">
