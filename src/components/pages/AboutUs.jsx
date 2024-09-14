e8204ea7ab9482e709d370a5e3728e3251af0456
tahsin soufizade <soufilearn@gmail.com>
2024-09-15 02:21:09 +0330

    add image to about-us page

--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -1,8 +1,12 @@
+import aboutUsImage from "../../assets/images/about-us-img.jpg";
 const AboutUs = () => {
   return (
     <div className="px-4 mt-8 dark:text-white text-center md:text-start">
       <h2 className="mb-5">
-        <span className="text-blue-300">تحسین صوفی زاده</span> هستم
+        <span className="dark:text-blue-300 text-blue-400">
+          تحسین صوفی زاده
+        </span>{" "}
+        هستم
       </h2>
       <h4>مدرس و برنامه نویس فرانت اند</h4>
       <hr className="my-5" />
@@ -10,6 +14,11 @@ const AboutUs = () => {
         <li className="py-1">۴ سال سابقه برنامه نویسی بصورت خودآموز</li>
         <li className="py-1">۲ سال سابقه تدریس فرانت اند بصورت حرفه ای</li>
       </ul>
+      <img
+        src={aboutUsImage}
+        alt="tahsin soufizade"
+        className="rounded-full w-4/6 border-2 border-blue-400 shadow-lg mx-auto mt-10"
+      />
     </div>
   );
 };
